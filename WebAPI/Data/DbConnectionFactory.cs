e67db5d71609afa56c43910fec6b558d29b62c44
Mauricio Bedoya <mauriyi@hotmail.com>
2024-10-02 13:10:54 -0500

    Initial commit: Set up project structure and establish database connection.

---
+++ WebAPI/Data/DbConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace WebAPI.Data
+{
+    public class DbConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            return new SqlConnection(connectionString);
+        }
+    }
+}
